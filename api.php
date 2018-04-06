0caa32150f34a87f681f8b5b6782c498a1ac16c3
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-06 07:37:13 +0000

    First running version of our API request to VinDecode API

---
+++ api.php
@@ -0,0 +1,57 @@
+<?php
+
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$input = json_decode(file_get_contents('php://input'),true);
+ 
+// create SQL based on HTTP method
+switch ($method) {
+  case 'GET':
+    echo $method."<br><br>";
+    $vin = $_GET["VIN"];
+    break;
+  case 'PUT':
+    echo $method."<br><br>";
+    break;
+  case 'POST':
+    echo $method."<br><br>";
+    echo print_r($input, true);
+    break;
+  case 'DELETE':
+    echo $method."<br><br>";
+    break;
+  default:
+    echo "wrong command...";
+    break;
+}
+ 
+$apiPrefix = "https://api.vindecoder.eu/2.0";
+$apikey = "7faaef90b983";   // Your API key
+$secretkey = "a3396cb5ac";  // Your secret key
+// $vin = ($_SERVER['VIN']) ? $_SERVER['VIN'] : "WAUZZZ4L0BD004645"; // Requested VIN
+echo "result for VIN: ".$vin;
+
+
+if($_SERVER['GET']==1) {
+
+  $id = $vin;
+  $controlsum = substr(sha1("{$id}|{$apikey}|{$secretkey}"), 0, 10);
+  $data = file_get_contents("{$apiPrefix}/{$apikey}/{$controlsum}/decode/{$vin}.json", false);
+
+} else {
+
+  $id = "info-".$vin;
+  $controlsum = substr(sha1("{$id}|{$apikey}|{$secretkey}"), 0, 10);
+  $data = file_get_contents("{$apiPrefix}/{$apikey}/{$controlsum}/decode/info/{$vin}.json", false);
+
+}
+
+$result = json_decode($data, true);
+
+echo "";
+echo print_r($result["decode"]);
+//echo print_r($result,true);
+echo "";
+
+?>
---
+++ restart.sh
@@ -0,0 +1,20 @@
+# phpapi
+
+## build:
+docker build -t vin2cert .
+
+
+## remove:
+docker stop vin2cert-app
+docker rm -fv vin2cert-app
+
+## run:
+docker run -d -p 8080:80 --name vin2cert-app vin2cert
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 5; done'
+
+echo "test case 1:"
+curl localhost:8080/api.php?VIN=WDD2210561A233135
+
+#echo "test case 2:"
+#curl -XPOST -H "Content-Type: application/json"  -d '{"username":"patrick","password":"mypw"}' localhost:8080/api.php?VIN=WDD2210561A233135
+
