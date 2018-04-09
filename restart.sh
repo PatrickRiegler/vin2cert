808d305a07d1582dbfed65ba57cfca9affd0faef
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-09 06:36:16 +0000

    switch to docker compose

--- restart.sh
+++ restart.sh
@@ -10,17 +10,22 @@ run ()
   $CMD
 }
 
+# set env var:
+export AWSIP=$(curl -s checkip.amazonaws.com)
 
 ## build:
 run "docker build -t vin2cert ."
 
 
 ## remove:
-run "docker stop vin2cert-app"
-run "docker rm -fv vin2cert-app"
+#run "docker stop vin2cert-app"
+#run "docker rm -fv vin2cert-app"
+run "docker-compose -f docker-compose.yml stop"
+run "docker-compose -f docker-compose.yml rm -vf"
 
 ## run:
-run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
+#run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
+run "docker-compose -f docker-compose.yml up -d"
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
 
 #run "echo 'test case 0:"'
