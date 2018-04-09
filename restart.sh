9027e172d9cf977e151ef6740e3e951b5e75c7a2
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-09 05:54:45 +0000

    implement run function and automatic IP registring

--- restart.sh
+++ restart.sh
@@ -1,20 +1,34 @@
 # phpapi
 
+
+run () 
+{
+  CMD=$1
+  echo "------------------------------"
+  echo "Running.. "
+  echo $CMD
+  $CMD
+}
+
+
 ## build:
-docker build -t vin2cert .
+run "docker build -t vin2cert ."
 
 
 ## remove:
-docker stop vin2cert-app
-docker rm -fv vin2cert-app
+run "docker stop vin2cert-app"
+run "docker rm -fv vin2cert-app"
 
 ## run:
-docker run -d -p 8080:80 --add-host="elasticsearch:18.184.37.127" --name vin2cert-app vin2cert
+run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
 
-echo "test case 1:"
-curl localhost:8080/api.php?VIN=WDD2210561A233135
+#run "echo 'test case 0:"'
+#run "curl -XPOST localhost:8080/api.php?VIN=WDD2210561A233135"
 
-#echo "test case 2:"
-#curl -XPOST -H "Content-Type: application/json"  -d '{"username":"patrick","password":"mypw"}' localhost:8080/api.php?VIN=WDD2210561A233135
+run 'echo "test case 1:"'
+run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
+
+#run 'echo "test case 2:"'
+#run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/api.php?VIN=WDD2210561A233135'
 
