b7e7da186be2cbf1086566236c78e4d78433eda4
Patrick Riegler <patrick@patricks-macbook-pro.home>
2018-04-18 13:16:16 +0200

    fix restart script issues

--- restart.sh
+++ restart.sh
@@ -11,7 +11,9 @@ run ()
 }
 
 # set env var:
-export AWSIP=$(curl -s checkip.amazonaws.com)
+if [ "${AWSIP}" == "" ]; then
+	export AWSIP=$(curl -s checkip.amazonaws.com)
+fi
 
 ## remove:
 run "docker-compose -f docker-compose.yml stop"
@@ -33,31 +35,31 @@ run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
 
 
 
-----------
-
-
-# old references...
-
-## build:
-run "docker build -t vin2cert ."
-
-
-## remove:
-#run "docker stop vin2cert-app"
-#run "docker rm -fv vin2cert-app"
-run "docker-compose -f docker-compose.yml stop"
-run "docker-compose -f docker-compose.yml rm -vf"
-
-## run:
-#run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
-run "docker-compose -f docker-compose.yml up -d"
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
-
-#run "echo 'test case 0:"'
-#run "curl -XPOST localhost:8080/api.php?VIN=WDD2210561A233135"
-
-run 'echo "test case 1:"'
-run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
-
-#run 'echo "test case 2:"'
-#run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/api.php?VIN=WDD2210561A233135'
+#----------
+#
+#
+## old references...
+#
+### build:
+#run "docker build -t vin2cert ."
+#
+#
+### remove:
+##run "docker stop vin2cert-app"
+##run "docker rm -fv vin2cert-app"
+#run "docker-compose -f docker-compose.yml stop"
+#run "docker-compose -f docker-compose.yml rm -vf"
+#
+### run:
+##run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
+#run "docker-compose -f docker-compose.yml up -d"
+##bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
+#
+##run "echo 'test case 0:"'
+##run "curl -XPOST localhost:8080/api.php?VIN=WDD2210561A233135"
+#
+#run 'echo "test case 1:"'
+#run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
+#
+##run 'echo "test case 2:"'
+##run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/api.php?VIN=WDD2210561A233135'
