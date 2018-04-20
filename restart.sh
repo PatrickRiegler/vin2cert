ed152068eb074cbab80812b9389722a8c145b292
Patrick Riegler <patrick@patricks-macbook-pro.home>
2018-04-20 10:27:05 +0200

    rename the api to vin2cert.php in client calls

--- restart.sh
+++ restart.sh
@@ -22,16 +22,16 @@ run "docker-compose -f docker-compose.yml rm -vf"
 
 ## run:
 run "docker-compose -f docker-compose.yml up -d --build"
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/vin2cert/vin2cert.php)" != "200" ]]; do echo $(date); sleep 1; done'
 
 #run "echo 'test case 0:"'
-#run "curl -XPOST localhost:8080/api.php?VIN=WDD2210561A233135"
+#run "curl -XPOST localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135"
 
 run 'echo "test case 1:"'
-run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
+run "curl localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135"
 
 #run 'echo "test case 2:"'
-#run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/api.php?VIN=WDD2210561A233135'
+#run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135'
 
 
 
@@ -54,13 +54,13 @@ run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
 ### run:
 ##run "docker run -d -p 8080:80 --add-host=elasticsearch:$(curl -s checkip.amazonaws.com) --name vin2cert-app vin2cert"
 #run "docker-compose -f docker-compose.yml up -d"
-##bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/api.php)" != "200" ]]; do echo $(date); sleep 1; done'
+##bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/vin2cert/vin2cert.php)" != "200" ]]; do echo $(date); sleep 1; done'
 #
 ##run "echo 'test case 0:"'
-##run "curl -XPOST localhost:8080/api.php?VIN=WDD2210561A233135"
+##run "curl -XPOST localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135"
 #
 #run 'echo "test case 1:"'
-#run "curl localhost:8080/api.php?VIN=WDD2210561A233135"
+#run "curl localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135"
 #
 ##run 'echo "test case 2:"'
-##run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/api.php?VIN=WDD2210561A233135'
+##run 'curl -XPOST -H "Content-Type: application/json"  -d \'{"username":"patrick","password":"mypw"}\' localhost:8080/vin2cert/vin2cert.php?VIN=WDD2210561A233135'
