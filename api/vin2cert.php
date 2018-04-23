64b6666c12165e5b7b66e4b7a2c3ff68d808e076
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-23 08:36:55 +0000

    add phpdebugbar to composer file and split vin2cert.php to lib.php

--- api/vin2cert.php
+++ api/vin2cert.php
@@ -1,16 +1,6 @@
 <?php
 
-$path = "/var/www/html";
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-
-require('vendor/autoload.php');
-
-use Elasticsearch\ClientBuilder;
-use WebSocket\Client;
-
-$client = new Client("ws://websocket:1337/");
-// $client->send("Hello ws!");
-// echo $client->receive(); // Will output 'Hello WebSocket.org!'
+require('./lib.php');
 
 function updateWebSocket($md5,$status,$result,$detail) {
   
