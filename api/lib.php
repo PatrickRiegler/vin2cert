0bba6e795d874fb918369016332fb5360d7f76f8
Patrick Riegler <patrick.riegler@intdev.ch>
2018-04-23 10:52:44 +0000

    split libraries into lib.php from vin2cert.php

--- api/lib.php
+++ api/lib.php
@@ -1,16 +1,93 @@
 <?php
 
+// extend path for including composer frameworks
 $path = "/var/www/html";
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
+// include composer frameworks
 require('vendor/autoload.php');
 
+// include and initialize PHPDebugBar: http://phpdebugbar.com/docs/
+use DebugBar\StandardDebugBar;
+
+$debugbar = new StandardDebugBar();
+$debugbarRenderer = $debugbar->getJavascriptRenderer();
+
+$debugbar["messages"]->addMessage("hello world!");
+
+// include elasticsearch framework
 use Elasticsearch\ClientBuilder;
 use WebSocket\Client;
 
-$client = new Client("ws://websocket:1337/");
-// $client->send("Hello ws!");
-// echo $client->receive(); // Will output 'Hello WebSocket.org!'
+// define elasticsearch hosts
+$hosts = [
+    'elasticsearch:9200'
+];
 
+// build elasticsearch client
+$client = ClientBuilder::create()           // Instantiate a new ClientBuilder
+                    ->setHosts($hosts)      // Set the hosts
+                    ->build();              // Build the client object
+
+// build websocket client
+$wsclient = new Client("ws://websocket:1337/");
+// $wsclient->send("Hello ws!");
+// echo $wsclient->receive(); // Will output 'Hello WebSocket.org!'
+
+// define the connection settings for the vindecoder.eu service
+$apiPrefix = "https://api.vindecoder.eu/2.0";
+$apikey = "7faaef90b983";   // Your API key
+$secretkey = "a3396cb5ac";  // Your secret key
+// $vin = ($_SERVER['VIN']) ? $_SERVER['VIN'] : "WAUZZZ4L0BD004645"; // Requested VIN
+// echo "result for VIN: ".$vin;
+
+
+// function for method not implemented
+function notImplemented($method) {
+    echo "Method: ".$method." is not implemented<br><br>";
+    exit(500);
+}
+
+function postResultToElastic($md5,$vin,$step,$stepDetail,$result,$detail) {
+  global $client;
+  $params = [
+      'index' => 'vin2cert',
+      'type' => 'my_type',
+      //'id' => $md5,
+      //'timestamp' => strtotime("-3h"),
+      'body' => [
+		'timestamp' => "'".strtotime("now")."'", 
+		'id' => "'".$md5."'",
+		'vin' => "'".$vin."'",
+		'step' => "'".$step."'",
+		'stepDetail' => "'".$stepDetail."'",
+		'result' => "'".$result."'",
+		'detail' => "'".$detail."'"
+		]
+  ];
+  $response = $client->index($params);
+  //print_r($response);
+}
+
+function updateWebSocket($md5,$vin,$step,$stepDetail,$result,$detail) {
+  global $wsclient;
+  // echo "$md5,$status,$result,$detail";
+  $arr = array(
+	'id' => $md5, 
+	'vin' => $vin, 
+	'step' => $step, 
+	'stepDetail' => $stepDetail, 
+	'result' => $result, 
+	'detail' => $detail
+      );
+  $wsclient->send(json_encode($arr));
+}
+
+function handleResult($md5,$vin,$step,$stepDetail,$result,$detail) {
+  // echo $step." - ".$stepDetail;
+  updateWebSocket($md5,$vin,$step,$stepDetail,$result,$detail);
+  postResultToElastic($md5,$vin,$step,$stepDetail,$result,$detail);
+
+}
 
 ?>
--- api/vin2cert.php
+++ api/vin2cert.php
@@ -2,42 +2,6 @@
 
 require('./lib.php');
 
-function updateWebSocket($md5,$status,$result,$detail) {
-  
-  echo $md5;
-   
-}
-
-
-$hosts = [
-    'elasticsearch:9200'
-];
-
-$client = ClientBuilder::create()           // Instantiate a new ClientBuilder
-                    ->setHosts($hosts)      // Set the hosts
-                    ->build();              // Build the client object
-
-function postResultToElastic($data) {
-
-  global $client;
-  $params = [
-      'index' => 'vin2cert',
-      'type' => 'my_type',
-      'id' => $vin,
-      //'timestamp' => strtotime("-3h"),
-      'body' => ['timestamp' => "'".strtotime("now")."'", 'response' => "'".$data."'"]
-  ];
-
-  $response = $client->index($params);
-  //print_r($response);
-
-}
- 
-$apiPrefix = "https://api.vindecoder.eu/2.0";
-$apikey = "7faaef90b983";   // Your API key
-$secretkey = "a3396cb5ac";  // Your secret key
-// $vin = ($_SERVER['VIN']) ? $_SERVER['VIN'] : "WAUZZZ4L0BD004645"; // Requested VIN
-// echo "result for VIN: ".$vin;
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
@@ -53,18 +17,16 @@ switch ($method) {
     if($vin=="") die("no VIN number specified...");
     break;
   case 'PUT':
-    // echo $method."<br><br>";
+    notImplemented($method);
     break;
   case 'POST':
-    // echo $method."<br><br>";
-    // echo print_r($input, true);
-    $vin = $_GET["VIN"];
+    notImplemented($method);
     break;
   case 'DELETE':
-    // echo $method."<br><br>";
+    notImplemented($method);
     break;
   default:
-    echo "wrong command...";
+    notImplemented($method);
     break;
 }
 
@@ -76,20 +38,18 @@ switch ($method) {
   $data = file_get_contents("{$apiPrefix}/{$apikey}/{$controlsum}/decode/info/{$vin}.json", false);
 
 $result = json_decode($data, true);
-updateWebSocket($md5,"status1","success","data");
+handleResult($md5,$vin,"vindecode","getFields","success",$result);
 
 // echo "";
 // echo print_r($result["decode"]);
 // echo "";
-
-// postResultToElastic($data);
-
 
   $id = $vin;
   $controlsum = substr(sha1("{$id}|{$apikey}|{$secretkey}"), 0, 10);
   $data = file_get_contents("{$apiPrefix}/{$apikey}/{$controlsum}/decode/{$vin}.json", false);
 
 $result = json_decode($data, true);
+handleResult($md5,$vin,"vindecode","getResult","success",$result);
 
 // echo "";
 // echo print_r($result["decode"]);
@@ -97,6 +57,6 @@ $result = json_decode($data, true);
 
 echo $data;
 
-#postResultToElastic($data);
+// postResultToElastic($data);
 
 ?>
