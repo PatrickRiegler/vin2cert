ceae8e853b5eb129c91680afb840c56b8ed0f786
Patrick Riegler <patrick@patricks-macbook-pro.home>
2018-06-07 12:28:52 +0200

    first draft for image2text

---
+++ image2text/image2text.php
@@ -0,0 +1,48 @@
+<?php
+    require 'vendor/autoload.php';
+
+    use Aws\Rekognition\RekognitionClient;
+
+    $options = [
+       'region'            => 'us-west-2',
+        'version'           => 'latest'
+    ];
+
+    $rekognition = new RekognitionClient($options);
+	
+    #Get local image
+    $photo = 'input.jpg';
+    $fp_image = fopen($photo, 'r');
+    $image = fread($fp_image, filesize($photo));
+    fclose($fp_image);
+
+
+    # Call DetectFaces
+    $result = $rekognition->DetectText(array(
+       'Image' => array(
+          'Bytes' => $image,
+       ),
+       'Attributes' => array('ALL')
+       )
+    );
+
+    print_r($result);
+    var_dumb($result);
+
+/*
+    # Display info for each detected person
+    print 'People: Image position and estimated age' . PHP_EOL;
+    for ($n=0;$n<sizeof($result['FaceDetails']); $n++){
+
+      print 'Position: ' . $result['FaceDetails'][$n]['BoundingBox']['Left'] . " "
+      . $result['FaceDetails'][$n]['BoundingBox']['Top']
+      . PHP_EOL
+      . 'Age (low): '.$result['FaceDetails'][$n]['AgeRange']['Low']
+      .  PHP_EOL
+      . 'Age (high): ' . $result['FaceDetails'][$n]['AgeRange']['High']
+      .  PHP_EOL . PHP_EOL;
+    }
+*/
+?>
+
+
